1fed0c5f2e273d1a326be71bdb8c6d01dfb8d075
Fake <34991992+fakeecity@users.noreply.github.com>
2022-10-03 17:01:02 -0400

    Filtered Song Length, Edited CSS

--- modules/resultsgrid.tsx
+++ modules/resultsgrid.tsx
@@ -4,9 +4,19 @@ export const formatResults = (p: any) => {
 
     const results = []
     for (var i = 0; i < p.length; i++) {
+        if(p[i].result.artist_names.length >= 25) {
+            p[i].result.artist_names = p[i].result.artist_names.substring(0,24) + '...'
+        }
+        if(p[i].result.title.length >= 25) {
+            p[i].result.title = p[i].result.title.substring(0,24) + '...'
+        }
+        if(p[i].result.release_date_with_abbreviated_month_for_display === null) {
+            p[i].result.release_date_with_abbreviated_month_for_display = '(N/A)'
+        }
+
         results.push(
         <div className={styles.inner}>
-            <a href={p[i].result.url} className={styles.thumbnail}><img src={p[i].result.song_art_image_thumbnail_url}/></a>
+            <a href={p[i].result.url} className={styles.thumbnail}><img className={styles.thumbnail} src={p[i].result.song_art_image_thumbnail_url}/></a>
             <div className={styles.mainData}>
                 <a href={p[i].result.url}>{p[i].result.title}</a>
                 <p>{p[i].result.artist_names}</p>
